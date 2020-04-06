fc381cec150a3d8df5de1a75158eab62a1fa1d19
Ivo Krastev <ivo.krustev@yahoo.com>
2020-04-06 12:11:56 +0300

    Updates

---
+++ precache-manifest.3fa726ae9dc9236ad08ed1011e43ec6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33144b081b7c60fa32d4808e0d2b2507",
+    "url": "/recipeProject/index.html"
+  },
+  {
+    "revision": "f584edfa31fb142028ac",
+    "url": "/recipeProject/static/css/main.3d8e1cdb.chunk.css"
+  },
+  {
+    "revision": "bf7bdd00ff99d12d7a76",
+    "url": "/recipeProject/static/js/2.76e5d01a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeProject/static/js/2.76e5d01a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f584edfa31fb142028ac",
+    "url": "/recipeProject/static/js/main.a8374545.chunk.js"
+  },
+  {
+    "revision": "0f0a592e35107cda4f3e",
+    "url": "/recipeProject/static/js/runtime-main.e9714a19.js"
+  },
+  {
+    "revision": "2e3d42d8317c148000135066a67adb23",
+    "url": "/recipeProject/static/media/lobster-webfont.2e3d42d8.ttf"
+  },
+  {
+    "revision": "62213730d7b317ea76a7bd5de4bab119",
+    "url": "/recipeProject/static/media/lobster-webfont.62213730.woff"
+  },
+  {
+    "revision": "74b19da9512452be7cdbfc347ad9f2b0",
+    "url": "/recipeProject/static/media/lobster-webfont.74b19da9.svg"
+  },
+  {
+    "revision": "dbf5d981dbc266f8fcc409d7635dda33",
+    "url": "/recipeProject/static/media/lobster-webfont.dbf5d981.eot"
+  }
+]);
